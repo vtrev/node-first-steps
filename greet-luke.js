eee2add38ccbd03eda0339ed671a48911770a243
vusi <baloyi.trev@gmail.com>
2018-07-25 12:55:00 +0200

    put the figlet message and styled message in one js file

--- greet-luke.js
+++ greet-luke.js
@@ -1,6 +1,16 @@
 const chalk = require('chalk');
+const figlet = require('figlet');
+
+
+
 
 const greet =  require('./greet');
 
 const styledMessage = chalk.bgBlack.green(greet('Luke'));
-console.log(styledMessage);
+const figletMessage = figlet.textSync('Hello Luke');
+
+
+console.log('styled message : ' + styledMessage);
+console.log('==============');
+console.log('figlet message : '+ figletMessage);
+
